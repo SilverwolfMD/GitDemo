62e7c49d0bde5ea4226b634eaa6577e476793c45
Fionn <dolphinsilverwolf@gmail.com>
2020-10-05 16:23:09 -0700

    Added buckets of bits

--- The New Stuff.py
+++ The New Stuff.py
@@ -1,3 +1,10 @@
 print("More stuff to add to the repository")
 
 print("this is another change to the new stuff")
+
+for i in range(256, 0, -1):
+    print("{} {}".format(hex(i), "buckets of bits on the bus"))
+    print("{} {}".format(hex(i), "buckets of bits"))
+    print("take one down")
+    print("short it to ground")
+    print("{} {}".format(hex(i - 1), "buckets of bits on the bus\n"))
